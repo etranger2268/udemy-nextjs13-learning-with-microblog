a4de16409d67f0df69cbf5eb1d6d0f3b83fe9b26
etranger2268 <shun.5.2268@gmail.com>
2026-02-18 23:32:41 +0900

    develop-4-16

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next';
 import '@/styles/globals.css';
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
 import { inter, notoSansJP } from '@/styles/fonts';
 
 export const metadata: Metadata = {
@@ -13,11 +15,15 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="ja">
       <body
-        className={`${inter.variable} ${notoSansJP.variable} font-sans antialiased bg-background`}
+        className={`${inter.variable} ${notoSansJP.variable} font-sans antialiased container mx-auto`}
       >
-        <header>Header</header>
+        <div>
+          <Header />
+        </div>
         <main>{children}</main>
-        <footer>Footer</footer>
+        <div>
+          <Footer />
+        </div>
       </body>
     </html>
   );
---
+++ src/components/Header.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+
+const Header = () => {
+  return (
+    <header className="flex justify-between items-center py-5 px-10 border-b border-gray-300">
+      <div>
+        <h1 className="text-2xl font-bold">
+          <Link href="/">Next.js 13 Blog</Link>
+        </h1>
+      </div>
+      <div>
+        <nav className="text-sm font-medium">
+          <Link href="/articles/new" className="hover:underline hover:text-sky-500">
+            New Post
+          </Link>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;

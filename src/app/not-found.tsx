bd7e567e2428188fcec14f2c7052dd6ea1b0087e
etranger2268 <shun.5.2268@gmail.com>
2026-02-19 23:13:31 +0900

    develop-5-34

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div className="h-[90vh] flex flex-col justify-center items-center">
+      <div className="space-y-8 text-center">
+        <h2 className="text-2xl font-bold text-red-500">404</h2>
+        <p className="text-lg font-medium">Not Found</p>
+      </div>
+    </div>
+  );
+}

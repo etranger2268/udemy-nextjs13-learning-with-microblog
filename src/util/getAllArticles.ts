dc8d0d2291a40c1f38bdcd567acd6bd961c30b03
etranger2268 <shun.5.2268@gmail.com>
2026-02-19 21:31:30 +0900

    develop-5-23

---
+++ src/util/getAllArticles.ts
@@ -0,0 +1,13 @@
+import type { Article } from '@/type/article';
+
+export const getAllArticles = async (): Promise<Article[]> => {
+  const apiURL = process.env.NEXT_PUBLIC_API_URL;
+  if (!apiURL) {
+    throw new Error('API URL is missing');
+  }
+  const res = await fetch(`${apiURL}/articles`);
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+  return res.json();
+};
